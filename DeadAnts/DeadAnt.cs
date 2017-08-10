cf0e2c75d2c836bc9cec34c1c553257fafdefd4f
xinyi <xinyi.kao@titansoft.com.sg>
2017-08-10 15:52:41 +0800

    add project

---
+++ DeadAnts/DeadAnt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadAnts
+{
+    public class DeadAnt
+    {
+        public int DeadAntCount(string ants)
+        {
+            if (string.IsNullOrEmpty(ants))
+            {
+                return 0;
+            }
+
+            var antWithoutLife = ants.Replace("ant", "").Replace(".", "").Replace(" ", "");
+            if (antWithoutLife.Trim().Length == 0)
+            {
+                return 0;
+            }
+            
+            return GetMaxDeadAntCount(antWithoutLife);
+        }
+
+        private int GetMaxDeadAntCount(string input)
+        {
+            var totalAntHead = input.Count(t => t == 'a');
+            var totalAntBody = input.Count(t => t == 'n');
+            var totalAntLeg = input.Count(t => t == 't');
+
+            if (totalAntHead >= totalAntBody && totalAntHead >= totalAntLeg)
+            {
+                return totalAntHead;
+            }
+            
+            return totalAntBody >= totalAntLeg ? totalAntBody : totalAntLeg;
+        }
+    }
+}
